ee86a8711a6f92beeaf15cf06840a29cbea78a42
B1tka7 <ss.foxovich.ss@gmail.com>
2026-05-01 21:19:03 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+import telebot
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+OMDB_API_KEY = os.getenv("OMDB_API_KEY")
+
+bot = telebot.TeleBot(BOT_TOKEN)
