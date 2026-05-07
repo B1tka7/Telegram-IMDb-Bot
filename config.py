1b294baa5a3cc4baf93f36d2ab3b9b617ea0ff1a
B1tka7 <ss.foxovich.ss@gmail.com>
2026-05-07 20:07:45 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,7 @@
 import os
 from dotenv import load_dotenv
-import telebot
 
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 OMDB_API_KEY = os.getenv("OMDB_API_KEY")
-
-bot = telebot.TeleBot(BOT_TOKEN)
