e7df2a1a52e15e3aa42b188991c41b34e2d84659
B1tka7 <ss.foxovich.ss@gmail.com>
2026-05-07 20:08:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-from config import bot
-import handlers
+import telebot
+from config import BOT_TOKEN
+from handlers import register_handlers
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+register_handlers(bot)
 
 bot.infinity_polling()
